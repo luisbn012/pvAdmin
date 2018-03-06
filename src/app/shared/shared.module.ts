e5c6896f99ab1c896e676a311228ed09fdbeb8c4
luisb <luisbn012@gmail.com>
2018-03-06 09:35:37 +0100

    Agregamos Módulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+
+import { NopageComponent } from './nopage/nopage.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+
+@NgModule({
+    declarations: [
+        NopageComponent,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent
+    ],
+    exports: [
+        NopageComponent,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent
+    ], 
+    imports:[]
+})
+
+export class SharedModule {}
